74c4c9a5eb9ca2bb63ca0ae3ca40c129144360f3
simone sanseverino <simonesanseverino45@gmail.com>
2026-05-21 17:47:06 +0200

    US3 - Phase 1-2: added career_request SQL script and CareerRequest model

--- sql/drop.sql
+++ sql/drop.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS users_roles;
-DROP TABLE IF EXISTS images;       -- <-- Inserita esattamente qui!
+DROP TABLE career_request;
+DROP TABLE IF EXISTS images;
 DROP TABLE IF EXISTS articles;
 DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS roles;
