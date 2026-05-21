893a15962cd20b04a28499e3cead88522d1af4d1
xhoni lickollari <lickollarixhoni@yahoo.it>
2026-05-21 18:13:05 +0200

    Added in Service EmailService & EmailServiceImpl

---
+++ src/main/java/it/aulab/progetto_finale_docente/services/EmailServiceImpl.java
@@ -0,0 +1,26 @@
+package it.aulab.progetto_finale_docente.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Override
+    @Async
+    public void sendSimpleEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("aulabpost@administration.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        javaMailSender.send(message);
+    }
+
+}
