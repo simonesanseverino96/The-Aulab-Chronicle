a9175fef0a868cad1920bac28f00ace0a1928bc0
simone sanseverino <simonesanseverino45@gmail.com>
2026-05-27 17:04:36 +0200

    feat: improved login.html and register.html UI | fix: added /css/** to public routes in SecurityConfig

--- src/main/java/it/aulab/progetto_finale_docente/config/SecurityConfig.java
+++ src/main/java/it/aulab/progetto_finale_docente/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
                                                 .requestMatchers("/register/**", "/", "/login",
                                                                 "/articles", "/images/**", "/articles/detail/**",
                                                                 "/categories/search/**", "/search/**",
-                                                                "/articles/search", "/operations/career/**")
+                                                                "/articles/search", "/operations/career/**", "/css/**")
                                                 .permitAll()
                                                 .anyRequest().authenticated())
                                 .formLogin(form -> form.loginPage("/login")
