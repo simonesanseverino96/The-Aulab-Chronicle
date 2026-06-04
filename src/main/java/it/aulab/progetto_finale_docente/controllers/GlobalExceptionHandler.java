a6889e972c47f4c3520df0875de83a8fcfdb0db8
simone sanseverino <simonesanseverino45@gmail.com>
2026-06-04 14:25:13 +0200

    Feat: increase multipart upload limits, add MaxUploadSizeExceededException handler

---
+++ src/main/java/it/aulab/progetto_finale_docente/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package it.aulab.progetto_finale_docente.controllers;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public String handleMaxUploadSizeExceededException(MaxUploadSizeExceededException e,
+            RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("errorMessage",
+                "File troppo grande! La dimensione massima consentita è 10MB per file");
+        return "redirect:/article/create";
+    }
+}
